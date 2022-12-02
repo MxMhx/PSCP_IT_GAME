5cc0a6b39ce6d05cdad2538134a3ff39ce734be8
MxMhx <91119555+MxMhx@users.noreply.github.com>
2022-12-02 18:25:29 +0700

    Add files via upload

---
+++ fern_game/main.py
@@ -0,0 +1,37 @@
+import pygame
+import os
+import random
+from dice import Dice
+from cat import Cat
+
+WIDTH, HEIGHT =  700, 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("cats and Ladders")
+
+WHITE = ((255, 255, 255))
+FPS = 60
+
+bg_image = pygame.image.load(os.path.join('assets', 'ladders.png'))
+bg_size = pygame.transform.scale(bg_image, (700, 700))
+
+cat = Cat()
+
+def draw2():
+    screen.fill(WHITE)
+    screen.blit(bg_size, (0, 0))
+    cat.draw(screen)
+    pygame.display.update()
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        cat.move()
+        draw2()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
